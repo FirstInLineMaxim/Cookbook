316cc16010af7ab8b3804243a0cf93e4c017b3d6
Frank Asamoah <frank.asamoah119@gmail.com>
2022-10-10 03:07:47 +0200

    added alert inline

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const iframeDisplay = document.querySelector("#myFrame");
 
 /* Share Recipe Button declaration */
-const recipeShareButton= document.querySelector('#share-recipe');
-
+const recipeShareButton = document.querySelector("#share-recipe");
 
 const pizzaTitle = document.querySelector(".pizza-title");
 const dumplingTitle = document.querySelector(".dumpling-title");
@@ -12,17 +11,15 @@ const hoverPizza = document.querySelector(".hover-pizza");
 const hoverDumpling = document.querySelector(".hover-dumpling");
 const hoverPork = document.querySelector(".hover-pork");
 
-
-
-const pizzaButton = document.querySelector(".pizza-btn");
-const dumplingButton = document.querySelector(".dumpling-btn");
-const porkButton = document.querySelector(".pork-btn");
+const pizzaButton = document.querySelector(".pizza--btn");
+const dumplingButton = document.querySelector(".dumpling--btn");
+const porkButton = document.querySelector(".pork--btn");
 
 // const pizzaRecipe = `<table class="table table-of-ingredients" id="pizza-btn">
 // <thead>
 //   <tr>
 //     <th >Ingredients</th>
-//     <th>Instructions</th> 
+//     <th>Instructions</th>
 //   </tr>
 // </thead>
 // <tbody>
@@ -49,22 +46,21 @@ const porkButton = document.querySelector(".pork-btn");
 // </tbody>
 // </table>`
 
-recipeShareButton.addEventListener('click', shareRecipe);
+recipeShareButton.addEventListener("click", shareRecipe);
 
 const shareRecipe = () => {
-  alert('Nice, we want ALL your Recipes!!!');
+  alert("Nice, we want ALL your Recipes!!!");
   // console.log("clicked");
-}
-
+};
 
 const handleClick = () => {
-  return iframeDisplay.src = "./recipes/pizza.html"
-}
+  document.getElementById("myFrame").src = "https://www.wikipedia.org/";
+};
 
-pizzaButton.addEventListener("click", handleClick)
+pizzaButton.addEventListener("click", handleClick);
 
 const handleHover = () => {
-  return displayTitle.innerHTML= `Pizza`
-}
+  return (displayTitle.innerHTML = `Pizza`);
+};
 
-hoverImage.addEventListener("mouseover", handleHover)
+hoverImage.addEventListener("mouseover", handleHover);
